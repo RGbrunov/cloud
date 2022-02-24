d9411dc40be2673399652717961409d8b69c55b1
josev <vallejosjhose@gmail.com>
2022-02-23 21:20:18 -0400

    update-inb2

--- inbranch.py
+++ inbranch.py
@@ -1,1 +1,15 @@
-update it
+# Encapsulation
+class Student:
+    def __init__(self):
+        self.__codesis = 2022   #private
+        self.codesis = 2020     #public
+
+    def getcodesis(self):
+        return self.__codesis
+
+    def __paymentaccount(self):
+        print("BNB-35336RRRRR")
+
+person = Student()
+print(person.__codesis)
+person._Student__paymentaccount()
